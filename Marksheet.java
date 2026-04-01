00dd1c273c11d7030f4366f71c8bfd4b78958c9c
sabirabanu10-jpg <sabirabanu10@gmail.com>
2026-04-01 18:13:50 +0400

    to show total & average

---
+++ Marksheet.java
@@ -0,0 +1,14 @@
+class Marksheet
+{
+	public static void main (String []args)
+	{
+		int mark1 =80;
+		int mark2 =90;
+		int mark3 = 70;
+		 int total = (mark1+mark2+mark3);
+		int average= total / 3;
+		
+		System.out.println( total);
+		System.out.println(average);
+	}
+}
