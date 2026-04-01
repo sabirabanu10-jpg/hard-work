6c9bdcc4c4769107b5a1e97e57d2bea9dcf1b7c3
sabirabanu10-jpg <sabirabanu10@gmail.com>
2026-04-01 17:08:01 +0400

    to show data variable

---
+++ Minedata.java
@@ -0,0 +1,17 @@
+class Minedata
+{
+	public static void main(String[]args)
+	{
+		String name =" sabi";
+		int    age= 31;
+		char grade ='B';
+		boolean passed = true;
+		
+		System.out.println( name);
+		System.out.println(  age);
+		System.out.println(  grade);
+		System.out.println( passed);
+	}
+}
+
+		
