fa2cedfed64b35b96bd807f7bec5b7f900275fd1
sabirabanu10-jpg <sabirabanu10@gmail.com>
2026-04-01 18:35:16 +0400

    to declare primidatatypes

---
+++ Primidata.java
@@ -0,0 +1,25 @@
+class Primidata
+{
+	public static void main(String[] args)
+	{
+		byte B=10;
+		short S=200;
+		char C='Z';
+		int I=5000;
+		long L =100000L;
+		float F =5.5F;
+		double D=123.456;
+		boolean Ok = false;
+		
+		System.out.println( B);
+
+		System.out.println( S);
+				System.out.println( C);
+						System.out.println( I);
+								System.out.println( L);
+										System.out.println( F);
+												System.out.println( D);
+														System.out.println( Ok);
+	}
+}
+	
